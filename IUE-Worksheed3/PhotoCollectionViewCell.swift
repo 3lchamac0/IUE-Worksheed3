3cf5df1688b3281a93a8e1f142b75ad4084b680f
vinodbhavya <vinodbhavya410@gmail.com>
2019-10-16 13:45:40 +0100

    clase 2

---
+++ IUE-Worksheed3/PhotoCollectionViewCell.swift
@@ -0,0 +1,14 @@
+//
+//  PhotoCollectionViewCell.swift
+//  IUE-Worksheed3
+//
+//  Created by formando on 16/10/2019.
+//  Copyright © 2019 formando. All rights reserved.
+//
+
+import UIKit
+
+class PhotoCollectionViewCell: UICollectionViewCell {
+    
+    @IBOutlet weak var imageView: UIImageView!
+}
